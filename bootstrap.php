5f6e6fb865778baa979050ba23d70858ef140fe8
sportsbyadrika <sportsbyadrika@gmail.com>
2025-11-10 16:39:21 +0530

    Improve bootstrap autoload handling

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,13 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+$autoloadPath = __DIR__ . '/vendor/autoload.php';
+
+if (!file_exists($autoloadPath)) {
+    throw new RuntimeException(
+        'Composer autoload file not found. Please run "composer install" to generate vendor/autoload.php.'
+    );
+}
+
+require_once $autoloadPath;
 
 use Dotenv\Dotenv;
 
